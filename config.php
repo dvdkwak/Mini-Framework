567df1a9496602c70d72bea0bbd2773fad21e913
David Kwakernaak <dkwakernaak@Davids-MacBook-Pro.local>
2020-02-10 10:59:21 +0100

    little spelling check

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 // if the application is within a sub-folder call it here, you will be able to use 'ROOT' for further references
-define("DIRECTORY", "/");
+define("DIRECTORY", "mini-framework");
 
-// defining the folder in which the assets are
+// defining the folder in which the assets are stored
 define("ASSETS_DIR", "assets");
 
 // if you want to be able to see php errors set this one to true (otherwise false of course)
