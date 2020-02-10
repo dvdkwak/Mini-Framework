259b60391ee1d19231f975478cf34195ab51f04e
David Kwakernaak <dkwakernaak@Davids-MacBook-Pro.local>
2020-02-10 10:57:44 +0100

    cleaning the config for general use witht he basic settings

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
 
-// if the application is within a subfolder call it here, you will be able to use 'ROOT' for further refferences
+// if the application is within a sub-folder call it here, you will be able to use 'ROOT' for further references
 define("DIRECTORY", "/");
+
+// defining the folder in which the assets are
+define("ASSETS_DIR", "assets");
 
 // if you want to be able to see php errors set this one to true (otherwise false of course)
 define("DEBUGMODE", true);
