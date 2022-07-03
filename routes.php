704210a8172410139ed226c468124a371ca48467
David Kwakernaak <davidkwakernaak@Davids-MacBook-Pro.local>
2022-07-03 20:56:37 +0200

    [routes udpate]: A small update on the default routes file.

--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@
 
 /**
  * In this area you add extra routes to your application, these routes look like:
- * $route->add('uri', 'view', 'controller');
+ * $route->add('uri', 'controller');
  */
 
 // Start editing here! :D (uncomment the line below)
