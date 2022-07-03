62031dcc1c6ce6f5fd66f5e6d4682b51f4104694
David Kwakernaak <davidkwakernaak@Davids-MacBook-Pro.local>
2022-07-03 19:46:56 +0200

    [new controller system]: Changed the process of how controllers are handled and added a controller base class to be able to create controllers more of a "mvc" way

---
+++ system/classes/Controller.class.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Raindrop;
+
+class Controller {
+  public function __construct($response) {
+    $this->response = $response;
+  }
+}
--- system/classes/Route.class.php
+++ system/classes/Route.class.php
@@ -27,41 +27,39 @@ class Route
     /**
      * Adding a route to the routes container which is used to create a page
      * @param string $path Used to define the uri which should be called
-     * @param string $view Used to define the view to render
      * @param null $controller Used to define which controller should be used
      */
-    public function add($path = "home", $view = "default.php", $controller = NULL)
+    public function add($path = "home", $controller = "default:main")
     {
-        // if the view is 'default.php' check wether to use the system default page or the user-made one
-        if($view === "default.php") {
-            if(!file_exists(ROOT . 'views/default.php')) {
-                $view = '../system/defaults/default.php';
+        // if the controller is 'default.php' check wether to use the system default page or the user-made one
+        if($controller === "default") {
+            if(!file_exists(ROOT . 'controllers/defaultController.php')) {
+                $controller = '../system/defaults/defaultController.php';
             }
         }
         // if there are no routes, the existence check is by default false
         if(empty($this->routes)) {
             $existsCheck = false;
-        }
-        // foreach existing route, check if the given path has already been defined
-        foreach($this->routes AS $key => $route) {
-            if($route['route'] === $path) {
-                // if it is, then we set the check to the value of the index of the route
-                $existsCheck = $key;
-            } else {
-                $existsCheck = false;
+        } else {
+            // foreach existing route, check if the given path has already been defined
+            foreach($this->routes AS $key => $route) {
+                if($route['route'] === $path) {
+                    // if it is, then we set the check to the value of the index of the route
+                    $existsCheck = $key;
+                } else {
+                    $existsCheck = false;
+                }
             }
         }
         // if there is a found route, we overwrite it on the existence key
         if($existsCheck !== false) {
             $this->routes[$existsCheck] = array(
                 "route" => $path,
-                "view" => $view,
                 "controller" => $controller
             );
         } else {
             $this->routes[] = array(
                 "route" => $path,
-                "view" => $view,
                 "controller" => $controller
             );
         }
@@ -69,12 +67,12 @@ class Route
 
 
     /**
-     * Creating a page according to the requested URI, the page is built up from the routes array
+     * Creating a response according to the requested URI, the page is built up from the routes array
      * @param array $urlParams Used to check what parameters are given by the uri
      * @param string $url the whole url used in the request
      * @return array The data needed to make a page in an array
      */
-    public function createPage($urlParams, $url)
+    public function response($urlParams, $url)
     {
         foreach($this->routes AS $key => $route){ // Foreach route
             $uri_path = explode("/", $route['route']); // create an array with all route params
@@ -98,15 +96,15 @@ class Route
             }
         }
         // When no key is set give the standard 404 error, but first we need to check if there is a user-made one (so in the views folder a 404.php)
-        if(file_exists(ROOT . 'views/404.php')) {
+        if(file_exists(ROOT . 'controllers/404.php')) {
             $data = array(
                 'route' => '404',
-                'view' => '404.php'
+                'controller' => 'default:error'
             );
         } else {
             $data = array( // Set the data to the 404
                 'route' => '404',
-                'view' => '../system/defaults/404.php',
+                'controller' => 'default:error',
             );
         }
         return $data;
